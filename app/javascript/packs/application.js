5e65f620ccbf232cd183f00d02b5bed6d19ae459
EC2 Default User <ec2-user@ip-172-31-22-225.ec2.internal>
2022-06-06 06:01:49 +0000

    星レビュー機能　実装途中

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,6 +2,7 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+//= require jquery
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
@@ -12,11 +13,18 @@ import "channels"
 import "jquery";
 import "popper.js";
 import "bootstrap";
-import "../stylesheets/application"
+import "../stylesheets/application";
 
 // fontawesome導入
-import '@fortawesome/fontawesome-free/js/all'
+import '@fortawesome/fontawesome-free/js/all';
 
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+
+Rails.start();
+Turbolinks.start();
+ActiveStorage.start();
+
+window.$ = window.jQuery = require('jquery');
+
+
+
+
