b74cc73628394d93ba07efd64833121f30720479
EC2 Default User <ec2-user@ip-172-31-22-225.ec2.internal>
2022-06-07 15:29:10 +0000

    カテゴリタグ、タグ検索フォーム　実装完了

--- app/models/book.rb
+++ app/models/book.rb
@@ -5,17 +5,20 @@ class Book < ApplicationRecord
   has_many :favorited_users, through: :favorites, source: :user
 
   has_many :book_comments, dependent: :destroy
-  
+
   has_many :view_counts, dependent: :destroy
+
+  has_many :book_tags, dependent: :destroy
+  has_many :tags, through: :book_tags
 
   validates :title, presence: true
   validates :body, presence: true, length: { maximum: 200 }
-  
+
   #データの取り出し方を指示
   scope :latest, -> {order(created_at: :desc)}  #latestは任意の名前、orderはデータの取り出し
   scope :old, -> {order(created_at: :asc)}      #created_atは投稿日カラム、descは昇順、ascは降順
   scope :star_count, -> {order(star: :desc)}
-  
+
   # 検索機能
   def self.search_for(content, method)
     if method == 'perfect'
@@ -31,5 +34,27 @@ class Book < ApplicationRecord
 
   def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
+  end
+
+  #タグ機能
+  def save_tags(savebook_tags)
+    # 現在のユーザーの持っているskillを引っ張ってきている
+    current_tags = self.tags.pluck(:name) unless self.tags.nil?
+    # 今bookが持っているタグと今回保存されたものの差をすでにあるタグとする。古いタグは消す。
+    old_tags = current_tags - savebook_tags
+    # 今回保存されたものと現在の差を新しいタグとする。新しいタグは保存
+    new_tags = savebook_tags - current_tags
+
+    # Destroy old taggings:
+    old_tags.each do |old_name|
+      self.tags.delete Tag.find_by(name:old_name)
+    end
+
+    # Create new taggings:
+    new_tags.each do |new_name|
+      book_tag = Tag.find_or_create_by(name:new_name)
+      # 配列に保存
+      self.tags << book_tag
+    end
   end
 end
